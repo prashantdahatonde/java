98fe456bd92ba648267026cf311f0a07335b55ed
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-02 00:10:41 +0530

    first commit

---
+++ ViratKohli.java
@@ -0,0 +1,18 @@
+package java;
+public class ViratKohli {
+    public static void main(String[] args) {
+        // Variables to store Virat Kohli's information
+        String name = "Virat Kohli";
+        int age = 36;
+        String team = "India";
+        int runs = 12000;
+        double battingAverage = 58.5;
+
+        // Print the information
+        System.out.println("Player: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Team: " + team);
+        System.out.println("Total Runs: " + runs);
+        System.out.println("Batting Average: " + battingAverage);
+    }
+}
