6bd20b88306c7cddf3c61c69dbfb25a4f5426cb3
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-03 21:52:33 +0530

    sum of number

---
+++ Sum.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Sum {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcom to your Calculator");
+        System.out.print("Please enter first number:");
+        int firstNum = input.nextInt();
+        System.out.print("Now , please enter second number:");
+        int secondNum = input.nextInt();
+        int sum = firstNum + secondNum;
+        System.out.println("Sum of your number is:"+sum);
+    }
+    
+}
