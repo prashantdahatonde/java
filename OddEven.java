d67e37df74deccbfd3d59b483482a6ad8871abf7
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-06 22:12:08 +0530

    odd even

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class OddEven {
+    
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the odd even calculator");
+        System.out.print("Please enter your number:");
+        int num = input.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Your number is even");
+        } 
+        else{
+              System.out.println("Your number is odd");
+        }
+    }
+}
