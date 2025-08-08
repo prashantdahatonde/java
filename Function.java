88ea324fc50aa5fe6bee8d681fd2b39ea7f444fc
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-08 16:41:26 +0530

    function

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Function {
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        greetUser();
+        System.out.println("Method calling complete");
+        greetUser();
+    }
+    public static void greetUser(){
+
+        System.out.println("Good morning from Prashant");
+    }
+}
