9fc4e7bd7aff11871d5464e802f05757f0ba47d4
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-13 21:52:46 +0530

    ARRAY

---
+++ Array.java
@@ -0,0 +1,16 @@
+ public class Array{
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        myArr[0] = 98;
+        myArr[1] = 65;
+        myArr[2] = 45;
+        myArr[3] = 23;
+        myArr[4] = 12;
+
+        System.out.println(myArr[0]);
+        System.out.println(myArr[1]);
+        System.out.println(myArr[2]);
+        System.out.println(myArr[3]);
+        System.out.println(myArr[4]);
+    }
+ }
