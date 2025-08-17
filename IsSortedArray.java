59f5f444f3f02559d41cfe54de63341ed6fce8c1
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-17 23:16:09 +0530

    SortedArray

---
+++ IsSortedArray.java
@@ -0,0 +1,32 @@
+ class IsSortedArray {
+    public static void main(String[] args){
+        System.out.println("Welcome to Array Sorting Check post\n");
+        int[] numArr = ArrayUtility.inputArray();
+        boolean isInc = isIncreasing(numArr);
+        boolean isDec = isDecreasing(numArr);
+
+    }
+
+    public static boolean isDecreasing(int[] numArr){
+        int i = 1;
+        while (i < numArr.length){
+            if (numArr[i] > numArr[i-1]){
+                return false;
+            }
+            i++
+        }
+        return true;
+    }
+
+    public static boolean isIncreasing(int[] numArr){
+        int i = 1;
+        while (i < numArr.length){
+            if (numArr[i] < numArr[i - 1]){
+                return false;
+
+            }
+            i++;
+        }
+        return true;
+    }
+}
