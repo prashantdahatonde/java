b93bd753e93a6ced8f74bf9d8af74f875ed3b704
prashantdahatonde <prashantdahatonde209@gmail.com>
2025-08-20 18:31:57 +0530

    EVENODDNUMBER

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+ class EvenOdd {
+    
+    public static void main(String[] args)
+     {
+        int no = 12;
+        if(no % 2 == 0)
+        {
+            System.out.println("The number is EVEN");
+        }
+        else
+        {
+            System.out.println("The number is ODD");
+        }
+
+
+
+        
+     }
+    
+}
